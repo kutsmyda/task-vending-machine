b3d294f94f5acaa1cf5f9f249997bcdddfa3d0c3
YaroslavKutsmyda <kutsmydadev@gmail.com>
2021-05-18 23:00:34 +0300

    Add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="page-footer #004d40 teal darken-4">
+            <div className="footer-copyright">
+                <div className="container">
+                    © 2014 Copyright Text
+                    <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = () => {
     return (
         <nav>
-            <div className="nav-wrapper #004d40 teal darken-4">
+            <div className="nav-wrapper #004d40 teal darken-3">
                 <a href="#" className="brand-logo">Vending Machine</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="badges.html">Controls</a></li>
-                    <li><a href="collapsible.html">Statistic</a></li>
+                    <li><a href="editing">Controls</a></li>
+                    <li><a href="/staticstic">Statistic</a></li>
                 </ul>
             </div>
         </nav>
