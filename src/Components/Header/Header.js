34b83c3f6dd0f26a8c9a1df714aaf58aad303e9f
YaroslavKutsmyda <kutsmydadev@gmail.com>
2021-05-21 08:31:08 +0300

    Add readme MD

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <nav>
             <div className="nav-wrapper #004d40 teal darken-3">
-                <Link to="/" className="brand-logo">Vending Machine Desktop</Link>
+                <Link to="/" className="brand-logo">Vending Snack Machine Desktop</Link>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><Link to={'/'}> List </Link></li>
                     <li><Link to={'/editing'}> Controls </Link></li>
